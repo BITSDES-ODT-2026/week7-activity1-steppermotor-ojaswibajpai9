c9c0bb6ad4aaa5bca275deb91873c06228043fe5
Ojaswi Bajpai <ojaswibajpai67@gmail.com>
2026-02-16 14:29:03 +0530

    Update StepperMotor-FullStep.py

--- StepperMotor-FullStep.py
+++ StepperMotor-FullStep.py
@@ -1,1 +1,37 @@
+from machine import Pin
+import time
 
+sm1 = Pin(5, Pin.OUT)
+sm2 = Pin(18, Pin.OUT)
+sm3 = Pin(19, Pin.OUT)
+sm4 = Pin(21, Pin.OUT)
+
+while True:
+
+    # 1100
+    sm1.on()
+    sm2.on()
+    sm3.off()
+    sm4.off()
+    time.sleep(0.005)
+
+    # 0110
+    sm1.off()
+    sm2.on()
+    sm3.on()
+    sm4.off()
+    time.sleep(0.005)
+
+    # 0011
+    sm1.off()
+    sm2.off()
+    sm3.on()
+    sm4.on()
+    time.sleep(0.005)
+
+    # 1001
+    sm1.on()
+    sm2.off()
+    sm3.off()
+    sm4.on()
+    time.sleep(0.005)
